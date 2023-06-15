50c8478b37df830946e3308840386f6fe66f5097
Koki Takayanagi <s1300141@u-aizu.ac.jp>
2023-06-15 17:14:00 +0900

    Greeting added

--- DiceGame.java
+++ DiceGame.java
@@ -1,6 +1,22 @@
 import java.util.Random;
+import java.util.Scanner;
+
+class Greeting{
+    String name;
+    Greeting(){
+	System.out.println("What is your name?");
+	System.out.print("> ");
+	Scanner sc = new Scanner(System.in);
+	name = sc.nextLine();
+	System.out.println("Hello, " + name + "!");
+    }
+}
+
 class DiceGame{
-    public static void main(String[] args){	
+    public static void main(String[] args){
+
+	Greeting people = new Greeting();
+	
 	Random rand = new Random();
 	System.out.println("Rolling dice...");
 	int sum = 0;
